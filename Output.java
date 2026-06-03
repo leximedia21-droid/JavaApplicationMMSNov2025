66af3f199ec463c4e9117c32c1d4c12b49eed510
leximedia21-droid <leximedia21@gmail.com>
2026-06-03 10:25:14 +0100

    Java Output

---
+++ Output.java
@@ -0,0 +1,13 @@
+public class Output{
+      public static void main(String[] args){
+	  System.out.println("hello everyone this is my java code");
+	  System.out.println("learning java is fun");
+      System.out.print("Here i am using the print method  ");
+	  System.out.print("This text will be appended");
+	  System.out.printf("%nMercy Ben is %d years old%n",25);
+	  System.out.printf("My account balance is %c%,2f%n",'$',7654999.922276);
+	  System.out.printf("%s is a boy%n","Benjamin");
+	  System.out.printf("Do you love java? %b%n",true);
+	  System.out.printf("Fuel price in Nigeria is %d%% %n",50);
+	}
+}
