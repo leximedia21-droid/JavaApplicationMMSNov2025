c9e51ecb83cf52092f96b00a308488ffcb8b2af4
leximedia21-droid <leximedia21@gmail.com>
2026-06-07 21:01:58 +0100

    java keyword

---
+++ DataType.java
@@ -0,0 +1,9 @@
+public class DataType{
+	public static void main(String[] args){
+		byte age = 25;
+		System.out.printf("you are %d years old %n", age);
+	
+		short quantity = 30000;
+		System.out.printf(" i bought %d of phone %n", quantity);
+   }
+}
